349edee090c4f27737906193489687573c53aa20
Srivastava <sahil.srivastava@worldline.com>
2024-08-07 14:59:11 +0530

    final commit

--- app/src/main/java/com/example/authentcate/utils/APIConsumer.kt
+++ app/src/main/java/com/example/authentcate/utils/APIConsumer.kt
@@ -11,7 +11,7 @@ import retrofit2.http.POST
 
 interface APIConsumer {
   // @POST("users/validate-unique-email")
-   // suspend fun validateEmailAddress(@Body body: ValidateEmailBody): retrofit2.Response<UniqueEmailValidationResponse>
+    // suspend fun validateEmailAddress(@Body body: ValidateEmailBody): retrofit2.Response<UniqueEmailValidationResponse>
 
     @POST("users/register")
     suspend fun registerUser(@Body body: RegisterBody): retrofit2.Response<AuthResponse>
